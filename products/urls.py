65db5d5057955aa604edd6e4aa5fbdd756e18247
Fabian Forero <fabianforero779@gmail.com>
2016-02-15 14:57:58 -0500

    Se teminan las funcionalidades del modulo productos CRUD y se define el paginador

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,13 @@ from django.conf.urls.static import static
 from . import views as prViews
 
 urlpatterns = [
-    url(r'^$', prViews.inicial_index, name='index'),
-    url(r'^lista-productos/$', prViews.ProductListView.as_view(),
-        name='lista-productos'),
+    #url(r'^$', prViews.inicial_index, name='index'),
+    url(r'^$', prViews.ProductListView.as_view(),
+        name='list'),
+    url(r'^product/(?P<pk>\d+)$', prViews.ProductDetail.as_view(), name='detail'),
+    url(r'^product/nuevo/$', prViews.ProductCreat.as_view(), name='new'),
+    url(r'^editar/(?P<pk>\d+)$', prViews.ProducrtUpdate.as_view(), name='edit'),
+    url(r'^borrar/(?P<pk>\d+)$', prViews.ProductDelete.as_view(), name='delete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, DetailView
+#from django.views.generic.detail import DetailView
+from django.views.generic.edit import (CreateView, UpdateView , DeleteView)
 
-from django.views.generic import ListView
-
-from .forms import (ProductForm, CategoryForm)
-from .models import (Category, Product)
+from .forms import ProductForm, CategoryForm
+from .models import Category, Product
 
 # Create your views here.
 
+title = 'Ejercicio Django con class-base views'
 
 def inicial_index(request):
     product = Product.objects.order_by('id')
@@ -24,18 +27,69 @@ def inicial_index(request):
 class ProductListView(ListView):
     model = Product
     template_name = 'products/prList.html'
-
-#    template_name = "books/publisher_detail.html"
+    paginate_by = 1
 
 #     def get(self, request, *args, **kwargs):
 #         self.object = self.get_object(queryset=Product.objects.all())
 #         return super(ProductListView, self).get(request, *args, **kwargs)
+#
+#     def get_queryset(self):
+#         return self.object.book_set.all()
 
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
-        context['title'] = 'Ejercicio Django - Productos'
+        context['title'] = title
         context['seconTitle'] = 'Listado de productos'
         return context
 
-#     def get_queryset(self):
-#         return self.object.book_set.all()
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'products/prDetail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        context['title'] = title
+        context['seconTitle'] = 'Detalle del producto'
+        return context
+
+
+class ProductCreat(CreateView):
+    model = Product
+    template_name = 'products/prFormulario.html'
+    success_url = reverse_lazy('productos:list')
+    fields = ['pr_name', 'pr_description', 'pr_image', 'pr_category']
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductCreat, self).get_context_data(**kwargs)
+        context['title'] = title
+        context['seconTitle'] = 'Crear nuevo producto'
+        context['bottonTitle'] = 'Crear Producto'
+        return context
+
+class ProducrtUpdate(UpdateView):
+    model = Product
+    template_name = 'products/prFormulario.html'
+    success_url = reverse_lazy('productos:list')
+    fields = ['pr_name', 'pr_description', 'pr_image', 'pr_category']
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProducrtUpdate, self).get_context_data(**kwargs)
+        context['title'] = title
+        context['seconTitle'] = 'Editar producto'
+        context['bottonTitle'] = 'Editar Producto'
+        return context
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'products/prDelete.html'
+    success_url = reverse_lazy('productos:list')
+#    fields = ['pr_name', 'pr_description', 'pr_image', 'pr_category']
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDelete, self).get_context_data(**kwargs)
+        context['title'] = title
+        context['seconTitle'] = 'Eliminar producto'
+        context['bottonTitle'] = 'Eliminar Producto'
+        return context
